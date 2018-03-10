4475dd714b736b7cf9d93380f35933779e7deb07
dzacek <kusionjacek@gmail.com>
2018-03-10 23:26:39 +0100

    last

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+console.log('test');
+});
+
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+	
+	var position = $($(this).attr("href")).offset().top;
+    console.log(position);
+
+	$("body, html").animate({
+		scrollTop: position
+	});
+});
